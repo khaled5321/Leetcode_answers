1fab924d8cb6ab5ef01aab491d24925094e5680b
Khaled Emad <khaledemad656@gmail.com>
2023-07-23 20:23:28 +0300

    Create 836_Rectangle_Overlap.py

---
+++ 836_Rectangle_Overlap.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isRectangleOverlap(self, rec1: List[int], rec2: List[int]) -> bool:
+        if(
+            rec2[1] >= rec1[3] or
+            rec2[3] <= rec1[1] or 
+            rec2[2] <= rec1[0] or
+            rec2[0] >= rec1[2]
+        ):
+            return False
+        else:
+            return True
